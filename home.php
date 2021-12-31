4bebfd9e63bca28532489de33db017059b9a85ce
DESKTOP-O2UQO9I\Aswath <aswath.chandrasekaran@stud.th-deg.de>
2021-12-31 16:32:53 +0100

    very basic homepage

--- home.php
+++ home.php
@@ -4,7 +4,25 @@
     require_once "./partials/navbar.php";
     $_SESSION['url'] = $_SERVER['REQUEST_URI']; 
 ?>
- 
+
+<section class="home">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col col-lg-6 col-sm-12 ps-0">
+                <div>
+                    <img src="ehr.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6 text">
+                <strong>Welcome to DR.EHR</strong><br>
+                Get your patient's healthrecord with a single click. <br>
+                <a class="btn btn-warning py-2 px-3 mt-3" href="./login/Login.php">Login</a> <span>or</span>
+                <a class="btn btn-primary py-2 mt-3" href="./Register/Register.php">Signup</a>
+            </div>
+        </div>
+        
+    </div>
+</section>
 
 
     <!-- <script src="./Js/index.js"></script> -->
