e9d651336b7be918ed2980011b6d014aba59e39a
DESKTOP-O2UQO9I\Aswath <aswath.chandrasekaran@stud.th-deg.de>
2022-01-04 19:17:42 +0100

    Deleted logincheck page

--- Login/Login.php
+++ Login/Login.php
@@ -3,6 +3,44 @@ $ROOT="../";
 include "../partials/header.php";
 require_once "../partials/navbar.php";
 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $email=$_POST["emailid"];
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailerror = "Please enter valid email address";
+    }else{
+        $statement=$conn->prepare("SELECT * FROM register WHERE Emailid=:email");
+        $statement->bindParam(':email',$email,PDO::PARAM_STR);
+        $statement->execute();
+        $logininfo=$statement->fetch(PDO::FETCH_ASSOC);
+        if($logininfo){
+        $password=password_verify($_POST['password'],$logininfo['Password']);
+        if($password){
+            $_SESSION['Doctor_id']=$logininfo['ID'];
+            $_SESSION['loggedin'] = "TRUE";
+            $_SESSION['username'] = $logininfo['Username'];
+            $_SESSION['Email']=$logininfo['Emailid'];
+            if($logininfo["Active"]){
+                if(isset($_SESSION['url'])){
+                    $url = $_SESSION['url']; 
+                }
+                else {
+                    $url = "../home.php"; 
+                }
+                header('Location:'.$url);  
+            }else{
+                $message= "The account hasn't been activated yet"; 
+            }
+        }else{
+            $message= "Userid or password doesn't match" ;
+        }
+        }else{
+        $message= "The username doesn't exist in the database"; 
+        }
+    }
+  }
+
+
+
 ?>
 <section class="Authorize">
     <div class="container  pt-2 ">
@@ -12,23 +50,23 @@ require_once "../partials/navbar.php";
                     <img class="card-image img-fluid" src="../drehr.png" alt="" >
                     <div class="card-body">
                     <h5 class="card-title">Login</h5>
-                    <?php if(isset( $_SESSION["message"]) && !empty( $_SESSION["message"])): ?>
-                            <div class="alert alert-warning alert-dismissible fade show " role="alert">
-                            <?php echo $_SESSION['message'];
-                                unset($_SESSION["message"]);
+                    <?php if(isset( $message) && !empty( $message)): ?>
+                            <div class="alert alert-danger alert-dismissible fade show " role="alert">
+                            <?php echo $message;
+                                unset($message);
                             ?>
                              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                             </div>
                     <?php endif; ?>    
-                        <form  action="./logincheck.php"method="POST">  
+                        <form  action="./login.php"method="POST">  
                             <div class="mb-3">
                                 <label for="emailid">Email id:</label>
                                 <input type="email" id="emailid" name="emailid" value="<?php if(isset($_POST["emailid"])) echo $_POST["emailid"]; ?>"  required autofocus>
                             </div>
-                            <?php if(isset($_SESSION['email-error'])): ?>
-                                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                    <?php echo $_SESSION['email-error'];
-                                        unset($_SESSION['email-error']);
+                            <?php if(isset($emailerror)): ?>
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <?php echo $emailerror;
+                                        unset($emailerror);
                                     ?>
                                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                 </div>
