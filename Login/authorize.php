7930624dccfcc9eb104528f56fa850d1820567c0
DESKTOP-O2UQO9I\Aswath <aswath.chandrasekaran@stud.th-deg.de>
2022-01-16 21:21:17 +0530

    no message

--- Login/authorize.php
+++ Login/authorize.php
@@ -1,5 +1,6 @@
-// Check whether user is logged in or not.
+
 <?php 
+// Check whether user is logged in or not.
 if(!(isset($_SESSION["loggedin"])  && $_SESSION["loggedin"] == "TRUE")) {
     $_SESSION["message"]="You must be logged in";
     header('Location: /EHR/Login/Login.php');
